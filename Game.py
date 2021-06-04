247c2ac645a6b175622a8af9069fd6963b4bc9a5
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2021-06-04 13:22:53 +0100

    Added Hangman States

--- Game.py
+++ Game.py
@@ -1,1 +1,18 @@
 
+def hangmanState (wrongGuesses):
+    states = {
+        0: """--------\n|      |\n|\n|\n|\n|""",
+        1: """--------\n|      |\n|      O\n|\n|\n|""",
+        2: """--------\n|      |\n|      O\n|      |\n|\n|""",
+        3: """--------\n|      |\n|      O\n|     \|\n|\n|""",
+        4: """--------\n|      |\n|      O\n|     \|/\n|\n|""",
+        5: """--------\n|      |\n|      O\n|     \|/\n|     /\n|""",
+        6: """--------\n|      |\n|      O\n|     \|/\n|     / \ \n|"""
+    }
+
+    print(states[wrongGuesses])
+
+hangmanState(6)
+
+
+    
